925694874b75594396e6db2e8cfa10cc87258b71
Lifeng Lin <linlifeng@gmail.com>
2025-07-12 15:08:04 -0700

    missed to register two routes in app init create_app function.

--- app/__init__.py
+++ app/__init__.py
@@ -13,16 +13,16 @@ def create_app():
 
     # Register Blueprints
     from .routes.stocks import stocks_bp
-    # from .routes.oligo import oligo_bp
-    # from .routes.vienna import vienna_bp
+    from .routes.oligo import oligo_bp
+    from .routes.vienna import vienna_bp
     # from .routes.image import image_bp
     # from .routes.wordcloud import wordcloud_bp
     # from .routes.art import art_bp
     # from .routes.chat import chat_bp
 
     app.register_blueprint(stocks_bp, url_prefix='/api/stocks')
-    # app.register_blueprint(oligo_bp, url_prefix='/api/oligo')
-    # app.register_blueprint(vienna_bp, url_prefix='/api/vienna')
+    app.register_blueprint(oligo_bp, url_prefix='/api/oligo')
+    app.register_blueprint(vienna_bp, url_prefix='/api/vienna')
     # app.register_blueprint(image_bp, url_prefix='/api/image')
     # app.register_blueprint(wordcloud_bp, url_prefix='/api/wordcloud')
     # app.register_blueprint(art_bp, url_prefix='/api/art')
